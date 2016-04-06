da64e4cc92c9fe2d5f7ec89f3b0b935e9085dd9d
h123456001 <justine@justinedemacbook-pro.local>
2016-04-06 16:58:55 +0800

    模仿好书项目，使用cocoapods 集成 leanCloud 然后新建个桥接文件bridging.h 在项目设置里搜索bring  找到oc  写入项目所在目录 界面用代码实现删除自带的viewcontroller 和 storyboard 创建各界面的文件夹 和 viewcontroller

---
+++ RentST/config.swift
@@ -0,0 +1,13 @@
+//
+//  config.swift
+//  RentST
+//
+//  Created by Justine on 16/4/6.
+//  Copyright © 2016年 hxb. All rights reserved.
+//
+
+import Foundation
+
+let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
+        
