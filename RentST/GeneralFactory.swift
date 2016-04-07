b5953543ab6596cad37fe1a8c99b02fb1ddafc51
h123456001 <justine@justinedemacbook-pro.local>
2016-04-07 17:28:51 +0800

    因为顶部navigation bar  每个页面都有 所以创建个工厂页面

---
+++ RentST/GeneralFactory.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralFactory.swift
+//  RentST
+//
+//  Created by Justine on 16/4/7.
+//  Copyright © 2016年 hxb. All rights reserved.
+//
+
+import UIKit
+
+class GeneralFactory: NSObject {
+
+}
